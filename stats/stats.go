2546e769344b56e0c639e5fa24f59bb57a3e1d30
Mariusz Gronczewski <xani666@gmail.com>
2022-08-30 18:53:27 +0200

    add frontend to backend traffic map

--- stats/stats.go
+++ stats/stats.go
@@ -9,19 +9,25 @@ const probes = 1024
 const interval = time.Second
 
 type Stats struct {
-	Frontends *StatBlock
-	Backends  *StatBlock
+	Frontends         *StatBlock
+	Backends          *StatBlock
+	FrontendToBackend map[string]*StatBlock
 }
 
 func New(ch chan haproxy.HTTPRequest) *Stats {
 	s := &Stats{
-		Frontends: newStatBlock(),
-		Backends:  newStatBlock(),
+		Frontends:         newStatBlock(),
+		Backends:          newStatBlock(),
+		FrontendToBackend: make(map[string]*StatBlock),
 	}
 	go func() {
 		for ev := range ch {
 			s.Frontends.Update(ev, ev.FrontendName)
 			s.Backends.Update(ev, ev.BackendName)
+			if _, ok := s.FrontendToBackend[ev.FrontendName]; !ok {
+				s.FrontendToBackend[ev.FrontendName] = newStatBlock()
+			}
+			s.FrontendToBackend[ev.FrontendName].Update(ev, ev.BackendName)
 		}
 	}()
 	return s
