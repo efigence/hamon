913a2f8226386924d1c026b68611ef1d6fe7d75e
Mariusz Gronczewski <xani666@gmail.com>
2022-09-12 16:00:27 +0200

    fix top rate

--- web/stats.go
+++ web/stats.go
@@ -11,19 +11,20 @@ import (
 func (b *WebBackend) V1TopRate(c *gin.Context) {
 	minRate, err := strconv.ParseFloat(c.Param("rate"), 64)
 	if err != nil {
-		c.String(http.StatusBadRequest, fmt.Sprintf("need valid rate in path[%s]", err))
+		c.String(http.StatusBadRequest, fmt.Sprintf("need valid filteredRate in path[%s]", err))
 	}
 	rate := b.stats.TopRate()
+	filteredRate := map[string]float64{}
 	for ip, ipRate := range rate {
 		if ipRate > minRate {
 			parsedIp := net.ParseIP(ip)
 			if parsedIp != nil {
-				v, _ := rate[ip]
-				rate[ip] = v + ipRate
+				v, _ := filteredRate[ip]
+				filteredRate[ip] = v + ipRate
 			}
 		}
 	}
 	c.JSON(http.StatusOK, gin.H{
-		"ip_rate": rate,
+		"ip_rate": filteredRate,
 	})
 }
